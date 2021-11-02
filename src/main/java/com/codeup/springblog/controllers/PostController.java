551d36d2f81d37c0b4bd14bf9124da9db05176d7
Javier Guerra <javier.j.guerra4@gmail.com>
2021-11-02 12:27:58 -0500

    Polished the code in PostController

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -12,24 +12,25 @@ public class PostController {
     @GetMapping("/posts")
     @ResponseBody
     public String indexPage() {
-
+        return "All post";
     }
 
     @GetMapping("/posts/{id}")
     @ResponseBody
-    public String individualPost(@PathVariable String id) {
-        return id;
+    public String individualPost(@PathVariable long id) {
+        return "Here is the post" + id;
     }
 
     @GetMapping("/posts/create")
     @ResponseBody
     public String formPost() {
-
+        return ;
     }
 
     @PostMapping("/post/create")
     @ResponseBody
     public String createPost() {
+            return ;
 
     }
 }
