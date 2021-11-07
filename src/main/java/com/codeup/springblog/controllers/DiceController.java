84b012295c0047ad079a4908d6dff776d888142a
Javier Guerra <javier.j.guerra4@gmail.com>
2021-11-07 10:15:46 -0600

    Updated diceController with random number message

--- src/main/java/com/codeup/springblog/controllers/DiceController.java
+++ src/main/java/com/codeup/springblog/controllers/DiceController.java
@@ -8,22 +8,33 @@ import java.util.Random;
 
 @Controller
 public class DiceController {
-    @GetMapping("/roll-dice")
-    public String diceRoll() { return "Dice";}
+    @GetMapping("/dice-roll")
+    public String diceRoll() { return "dice-roll";}
 
-    @RequestMapping(path = "/roll-dice/{n}", method = RequestMethod.GET)
-    @ResponseBody
-    public String numberGuessed(@PathVariable int n, Model model){
-        Number guess = new Number(n, )
-        Random rand = new Random();
-        int upperbound = 7;
-        int int_random = rand.nextInt(upperbound);
-        if( n != int_random) {
-            return "You didn't guess correct. The guess was " + int_random + " and you chose " +
-                    n;
-        } else {
-            return "You guessed correct!";
-        }
-        model.addAttribute("guess", n);
+    @GetMapping("/dice-roll/{number}")
+    public String showRoll(@PathVariable String number, Model model){
+        int randomRoll = (int) (Math.random() * 6) + 1 ;
+        boolean guessedRight = Integer.parseInt(number) == randomRoll;
+        model.addAttribute("roll", randomRoll);
+        model.addAttribute("guess", number);
+        model.addAttribute("guessedRight", guessedRight);
+        return "dice-roll";
     }
+
+//    @RequestMapping(path = "/roll-dice/{n}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String numberGuessed(@PathVariable int n, Model model){
+//        Number guess = new Number(n, )
+//        Random rand = new Random();
+//        int upperbound = 7;
+//        int int_random = rand.nextInt(upperbound);
+//        if( n != int_random) {
+//            return "You didn't guess correct. The guess was " + int_random + " and you chose " +
+//                    n;
+//        } else {
+//            return "You guessed correct!";
+//        }
+//        model.addAttribute("guess", n);
+//    }
+
 }
