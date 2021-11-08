4f8f4372dac071c2b99a9f415a11def38de256f1
Javier Guerra <javier.j.guerra4@gmail.com>
2021-11-08 09:45:36 -0600

    Form model binding with coffee, coffee controller, and coffeeRepository

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -2,16 +2,21 @@ package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Coffee;
 import com.codeup.springblog.models.Coffee;
+import com.codeup.springblog.repositories.CoffeeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CoffeeController {
+    private final CoffeeRepository coffeeRepository;
+
+    public CoffeeController(CoffeeRepository coffeeRepository){
+        this.coffeeRepository = coffeeRepository;
+    }
     @GetMapping("/coffee")
     public String coffeeInfo(){
         return "views-lec/coffee";
@@ -38,5 +43,20 @@ public class CoffeeController {
         model.addAttribute("selections", selections);
         return "views-lec/coffee";
     }
+
+    @GetMapping("/coffee/create")
+    public String showCreateCoffeeForm(){
+        return "coffees/create";
+    }
+
+    @PostMapping("/coffee/create")
+    public String createCoffee(@RequestParam(name = "brand") String brand,
+        @RequestParam(name = "origin") String origin, @RequestParam(name = "roast") String roast) {
+        Coffee coffee= new Coffee(brand, origin, roast);
+        coffeeRepository.save(coffee);
+        return "redirect: /views-lec/coffee";
+    }
+
+
 }
 
--- src/main/java/com/codeup/springblog/models/Coffee.java
+++ src/main/java/com/codeup/springblog/models/Coffee.java
@@ -1,8 +1,34 @@
 package com.codeup.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "coffee")
 public class Coffee {
+
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    public Coffee(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    @Column(nullable = false, length = 6)
     private String roast;
+    @Column(nullable = false, length = 50)
     private String origin;
+    @Column(nullable = false, length = 50)
     private String brand;
 
     public String getBrand() {
@@ -19,6 +45,12 @@ public class Coffee {
     public Coffee(String roast, String brand) {
         this.roast = roast;
         this.brand = brand;
+    }
+
+    public Coffee(String brand, String origin, String roast) {
+        this.brand = brand;
+        this.origin = brand;
+        this.roast = roast;
     }
 
     public String getRoast() {
